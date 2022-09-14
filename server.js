29581aeda774fd34003740aeceb90d252d7bc4c7
Descotto <davidescotto@hotmail.com>
2022-09-13 21:59:54 -0700

    add: google translate api

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('./config/ppConfig');
 const isLoggedIn = require('./middleware/isLoggedIn');
+const axios = require('axios');
 
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
 console.log('works ', SECRET_SESSION);
 
+const encodedParams = new URLSearchParams();
+encodedParams.append("q", "Hello, world!");
+encodedParams.append("target", "es");
+encodedParams.append("source", "en");
 
 app.set('view engine', 'ejs');
 
@@ -38,12 +43,30 @@ app.use((req, res, next) => {
   next();
 });
 
+const apiKey = process.env.APIKEYLOG;
 
+const options = {
+  method: 'POST',
+  url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error('Crash :', error);
+});
 
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 
 app.use('/auth', require('./controllers/auth'));
 
